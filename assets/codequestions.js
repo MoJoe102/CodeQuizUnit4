50866b20bfe99ed835c8df244e66a6b2143bd27b
MoJoe102 <72671999+MoJoe102@users.noreply.github.com>
2020-11-29 04:51:47 -0600

    Add files via upload

---
+++ assets/codequestions.js
@@ -0,0 +1,35 @@
+// Code Challenge Questions
+let questions = [
+    {
+        title: "let allows you to declare what?",
+        choices: ["functions", "arguments", "booleans", "variables"],
+        answer: "variables"
+    },
+    {
+        title: "The Conditional In An If / Else Statment Is Enclosed Within _____.",
+        choices: ["Parentheses", "Quotes", "Curly-Brackets", "Square-Brackets"],
+        answer: "Parentheses"
+    },
+    {
+        title: "Arguments is a - object accessible inside functions that contain values of the arguments passed to the function",
+        choices: ["Object", "Array", "Conditonal", "Parameter",],
+        answer: "Array"
+    },
+    {
+        title: "What JavaScipt Method Is Used to Select an HTML Element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What HTML tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What Attribute Is Used in Html to Decorate Content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    }
+    
+    
+]
